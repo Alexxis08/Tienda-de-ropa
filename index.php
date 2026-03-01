9c9b72a245bf481d2224fe652db0e65638e4fd97
Alexis Juarez <alexjuarez1902@gmail.com>
2026-03-01 17:59:50 -0600

    refactor: crear archivo para prendas

--- index.php
+++ index.php
@@ -7,52 +7,7 @@
     <title>Tienda</title>
 </head>
 <body>
-<?php 
-    $prendas = [
-        [
-        "nombre" => "Playera Guess",
-        "marca" => "Guess",
-        "descripcion" => "Playera para cualquier ocacion",
-        "imagen" => "./Assets/recurso.jpeg",
-        "precio" => "350",
-        ],
-        [
-        "nombre" => "Playera Tommy",
-        "marca" => "Tommy",
-        "descripcion" => "Playera para cualquier ocacion",
-        "imagen" => "./Assets/recurso.jpeg",
-        "precio" => "450",
-        ],
-        [
-        "nombre" => "Playera XD",
-        "marca" => "No se",
-        "descripcion" => "Playera para cualquier ocacion",
-        "imagen" => "./Assets/recurso.jpeg",
-        "precio" => "150",
-        ],
-        [
-        "nombre" => "Short",
-        "marca" => "Guess",
-        "descripcion" => "Short para cualquier ocacion",
-        "imagen" => "./Assets/recurso.jpeg",
-        "precio" => "300",
-        ],
-        [
-        "nombre" => "Mini falda cachonda",
-        "marca" => "Path",
-        "descripcion" => "Playera para cualquier ocacion",
-        "imagen" => "./Assets/recurso.jpeg",
-        "precio" => "500",
-        ],
-        [
-        "nombre" => "Sueter Hugo",
-        "marca" => "Hugo Boss",
-        "descripcion" => "Playera para cualquier ocacion",
-        "imagen" => "./Assets/recurso.jpeg",
-        "precio" => "750",
-        ],
-    ]
-?>
+<?php include_once "prendas.php" ?>
 
 <h1>Tienda de ropa: </h1>
 
